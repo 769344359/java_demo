9ca126f0b85f6eab69ff3de240601a28299e33c8
daixiao.liu <thedinosaurmail@gmail.com>
2023-02-08 08:13:13 +0800

    never stop example

---
+++ LoopMayNeverEnd/LoopMayNeverEnd.java
@@ -0,0 +1,26 @@
+public class LoopMayNeverEnd {
+    boolean done = false;
+
+    public LoopMayNeverEnd() {
+    }
+
+    public void work() {
+        while(!this.done) {
+        }
+
+    }
+
+    public void stopWork() {
+        this.done = true;
+    }
+
+    public static void main(String[] argv) throws Exception {
+        LoopMayNeverEnd obj = new LoopMayNeverEnd();
+        Thread thread = new Thread(() -> obj.work());
+        thread.start();
+        Thread.sleep(1000L);
+        System.out.print("after sleep");
+        Thread thread2 = new Thread(() -> obj.stopWork());
+        thread2.start();
+    }
+}
