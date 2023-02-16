3ac7b456331c7dae66355da1e1e3eca04e8ec53f
daixiao.liu <thedinosaurmail@gmail.com>
2023-02-17 07:10:10 +0800

    添加例子

--- LoopMayNeverEnd/Visiable.java
+++ LoopMayNeverEnd/Visiable.java
@@ -1,12 +1,12 @@
 class Visible{
     public static  int counter = 0 ; 
     public static  void main(String[]  argc){
-        Runnable runnable = () -> counter++ ;
-        Runnable runnable2 = () -> {
+        Runnable runnable = () -> counter++ ;   // 线程1 自增
+        Runnable runnable2 = () -> {   
             
             try{
                 Thread.sleep(1000);
-                System.out.print(counter);
+                System.out.print(counter);    // 线程2 看到线程1的自增 ， 输出是1
             }catch(Exception exception){
 
             }
