0139eceb9f8f4fa0c3956031f551fd6f1a8fd49b
daixiao.liu <thedinosaurmail@gmail.com>
2023-02-17 07:11:41 +0800

    添加注释

--- LoopMayNeverEnd/LoopMayNeverEnd.java
+++ LoopMayNeverEnd/LoopMayNeverEnd.java
@@ -10,11 +10,11 @@ public class LoopMayNeverEnd {
 
     public static void main(String[] argv) throws Exception {
         LoopMayNeverEnd obj = new LoopMayNeverEnd();
-        Thread thread = new Thread(() -> obj.work());
+        Thread thread = new Thread(() -> obj.work());  // 线程1 死循环
         thread.start();
         Thread.sleep(1000L);
         System.out.print("after sleep");
-        Thread thread2 = new Thread(() -> obj.stopWork());
+        Thread thread2 = new Thread(() -> obj.stopWork());  // 线程2 修改标志位
         thread2.start();
     }
 }
