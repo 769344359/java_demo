75066195a5a324e453ca81d8d3122f6726da4e32
刘代晓 <daixiao.liu@patpat.com>
2023-02-17 13:05:05 +0800

    添加常量折叠例子

---
+++ folder/Folder.java
@@ -0,0 +1,7 @@
+
+public class Folder {
+    public static void main(String [] argv){
+        String demo = "a" + "b";
+    }
+    
+}
---
+++ folder/build.sh
@@ -0,0 +1,7 @@
+## compile
+javac folder/Folder.java   -d target
+
+
+## get asm
+
+javap -c  target/Folder.class
